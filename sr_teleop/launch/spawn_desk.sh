c2542f5d501fb12fc864dfc1da83920e00dadc55
Robert Krug <robert.krug@oru.se>
2012-03-12 22:07:53 +0100

    sr_teleop: fixed the desk.urdf - the desk model is now static

--- sr_teleop/launch/spawn_desk.sh
+++ sr_teleop/launch/spawn_desk.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rosparam set -t `rospack find sr_teleop`/robot/desk.urdf desk_description
-rosrun gazebo spawn_model -file `rospack find sr_teleop`/robot/desk.xml -gazebo -x 0.8 -y -0.1 -z 0.05 -model desk
+rosrun gazebo spawn_model -param desk_description -urdf -x 0.8 -y -0.1 -z 0.001 -model desk
 rosrun tf static_transform_publisher 0 0 0 0 0 0 world desk_plate 100
 
 
