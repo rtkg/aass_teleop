d851c57144bf984dc73b0309a8ccb723f2aa25d3
Robert Krug <robert.krug@oru.se>
2012-03-24 17:43:39 +0100

    sr_teleop: Added support for the ellipsoid version of the shadow hand

--- sr_teleop/launch/spawn_desk.sh
+++ sr_teleop/launch/spawn_desk.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rosparam set -t `rospack find sr_teleop`/robot/desk.urdf desk_description
 rosrun gazebo spawn_model -param desk_description -urdf -x 0.8 -y -0.1 -z 0.001 -model desk
-rosrun tf static_transform_publisher 0 0 0 0 0 0 world desk_plate 100
+rosrun tf static_transform_publisher 0.8 -0.1 0.001 0 0 0 fixed desk_plate 100
 
 
 
